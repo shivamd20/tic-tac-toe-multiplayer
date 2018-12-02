2292a3f8d4314156675042cc472277f0117bed3b
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2018-12-02 14:38:02 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
