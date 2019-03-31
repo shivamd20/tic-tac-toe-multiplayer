ba4c6a737852d0e98ddd1cd6125eeb94796291f9
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2019-03-31 15:14:15 +0530

    subscriprtion fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,14 @@ import { StartGameComponent } from './start-game/start-game.component';
 import { FormsModule } from '@angular/forms';
 
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { HttpLinkModule, HttpLink, HttpLinkHandler } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { TestGraphComponent } from './test-graph/test-graph.component';
+import {WebSocketLink} from 'apollo-link-ws';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
 
 const routes: Routes = [
   {
@@ -30,6 +35,7 @@ const routes: Routes = [
   }
 ];
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,24 +49,62 @@ const routes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    ApolloModule,
+    HttpLinkModule
 
   ],
   providers: [{
     provide: APOLLO_OPTIONS,
     useFactory: (httpLink: HttpLink) => {
+      const handler: HttpLinkHandler = httpLink.create({
+        uri: 'https://ramu420.herokuapp.com/v1alpha1/graphql',
+        headers: new HttpHeaders({'x-hasura-admin-secret': 'ramu'})
+      });
 
-    const  headers = new HttpHeaders();
+      // const wsLink = new WebSocketLink({
+      //   uri: `wss://ramu420.herokuapp.com/v1alpha1/graphql`,
+      //   options: {
+      //     lazy: true,
+      //     reconnect: true,
+      //     connectionParams: async () => {
+      //       return {
+      //         headers: {
+      //           'x-hasura-admin-secret': 'ramu',
+      //         },
+      //       }
+      //     },
+      //   },
+      // })
 
-    headers.append('x-hasura-admin-secret', 'ramu');
+      const WS_URL = `wss://ramu420.herokuapp.com/v1alpha1/graphql`;
 
+      const wsLink = new WebSocketLink(
+        new SubscriptionClient(WS_URL, {
+          reconnect: true,
+          timeout: 9000,
+          connectionParams: {
+            headers: {
+              'x-hasura-admin-secret': 'ramu',
+            }
+          }
+        })
+      );
+
+      const link = split(
+        // split based on operation type
+        ({ query }) => {
+          const { kind, operation } = getMainDefinition(query);
+          console.log(kind, operation, query)
+          return kind === 'OperationDefinition' && operation === 'subscription';
+        },
+       wsLink,
+        handler,
+      );
       return {
         cache: new InMemoryCache(),
-        link: httpLink.create({
-          uri: 'https://ramu420.herokuapp.com/v1alpha1/graphql',
-          headers
-        })
-      }
+        link: link,
+      };
     },
     deps: [HttpLink]
   }],
