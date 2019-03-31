1d96db19a8acccbb1b1eda3ef89585b6ef84d327
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2019-03-31 05:49:18 +0530

    apollo setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { CellComponent } from './cell/cell.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
 import { StartGameComponent } from './start-game/start-game.component';
 import { FormsModule } from '@angular/forms';
+
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const routes: Routes = [
   {
@@ -26,7 +30,7 @@ const routes: Routes = [
     AppComponent,
     BoardComponent,
     CellComponent,
-    StartGameComponent
+    StartGameComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +40,24 @@ const routes: Routes = [
     FormsModule
 
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+
+    const  headers = new HttpHeaders();
+
+    headers.append('x-hasura-admin-secret', 'ramu');
+
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'https://ramu420.herokuapp.com/v1alpha1/graphql',
+          headers
+        })
+      }
+    },
+    deps: [HttpLink]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
